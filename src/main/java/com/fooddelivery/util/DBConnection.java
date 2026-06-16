3df617c8c8502b5ec93e2c845df9e9b9f14b8d37
Nithin <gnithin1807gmail@gmail.com>
2026-06-16 20:50:18 +0530

    Use environment variables for DB connection

--- src/main/java/com/fooddelivery/util/DBConnection.java
+++ src/main/java/com/fooddelivery/util/DBConnection.java
@@ -5,22 +5,26 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL =
-            "jdbc:mysql://turntable.proxy.rlwy.net:33453/food_delivery";
-
-    private static final String USER = "root";
-
-    private static final String PASSWORD =
-            "icyccqShjrFDoBwTVwBxHAfpwhHdiJuY";
-
     public static Connection getConnection() {
         Connection con = null;
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            String host = System.getenv("DB_HOST");
+            String port = System.getenv("DB_PORT");
+            String db = System.getenv("DB_NAME");
+            String user = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASSWORD");
+
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + db;
+
+            con = DriverManager.getConnection(url, user, password);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return con;
     }
 }
